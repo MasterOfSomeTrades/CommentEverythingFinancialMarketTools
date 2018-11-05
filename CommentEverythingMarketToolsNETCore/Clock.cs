b01cc8ad1922613b8695fe57829801a56924b904
MasterOfSomeTrades <amyas@rogers.com>
2018-11-04 20:54:30 -0500

    Initial files

---
+++ CommentEverythingMarketToolsNETCore/Clock.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace CommentEverythingMarketToolsNETCore {
+    static class Clock {
+        public static bool IsNowInTimeRange(int startHour, int startMinute, int startSecond, int endHour, int endMinute, int endSecond) {
+            bool ret = false;
+
+            TimeSpan start = new TimeSpan(startHour, startMinute, startSecond); // --- 10, 0, 0 = 10 o'clock
+            TimeSpan end = new TimeSpan(endHour, endMinute, endSecond); // --- 12, 0, 0 = 12 o'clock
+
+            DateTime timeUtc = DateTime.UtcNow;
+            TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            DateTime easternTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, easternZone);
+            TimeSpan nowInEasternTime = easternTime.TimeOfDay;
+
+            if ((nowInEasternTime > start) && (nowInEasternTime < end)) {
+                ret = true;
+            } else {
+                ret = false;
+            }
+
+            return ret;
+        }
+
+        public static bool IsNowInTimeRange(TimeSpan startTime, TimeSpan endTime) {
+            bool ret = false;
+
+            DateTime timeUtc = DateTime.UtcNow;
+            TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            DateTime easternTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, easternZone);
+            TimeSpan nowInEasternTime = easternTime.TimeOfDay;
+
+            if ((nowInEasternTime > startTime) && (nowInEasternTime < endTime)) {
+                ret = true;
+            } else {
+                ret = false;
+            }
+
+            return ret;
+        }
+
+        public static class TimeZoneId {
+            public const string PACIFIC_STANDARD_TIME = "Pacific Standard Time";
+            public const string CENTRAL_STANDARD_TIME = "Central Standard Time";
+            public const string EASTERN_STANDARD_TIME = "Eastern Standard Time";
+            public const string UTC = "UTC";
+            public const string GREENWICH_MERIDIAN_TIME = "GMT";
+        }
+
+        public static DateTime Convert(DateTime utcTime, string timeZone) {
+            TimeZoneInfo convertToTimeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZone);
+
+            return TimeZoneInfo.ConvertTimeFromUtc(utcTime, convertToTimeZone);
+        }
+
+        public static int ConvertToUnixTimestamp(DateTime time, string fromTimeZone) {
+            DateTime utcDateTime = TimeZoneInfo.ConvertTimeToUtc(time, TimeZoneInfo.FindSystemTimeZoneById(fromTimeZone));
+            return (int) utcDateTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+        }
+
+        public static DateTime Convert(double unixTimestamp, string timeZone) {
+            // Unix timestamp is seconds past epoch
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimestamp);
+            TimeZoneInfo convertToTimeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZone);
+
+            return TimeZoneInfo.ConvertTimeFromUtc(dtDateTime, convertToTimeZone);
+        }
+
+        public static DateTime GetCurrentTimeByTimeZone(string timeZone) {
+            DateTime timeUtc = DateTime.UtcNow;
+            TimeZoneInfo convertToTimeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZone);
+
+            return TimeZoneInfo.ConvertTimeFromUtc(timeUtc, convertToTimeZone);
+        }
+
+        public static string DisplayMonthDayTimeString(DateTime time) {
+            return time.ToString("MMMdd HH:mm");
+        }
+
+        public static string DisplayDateString(DateTime time) {
+            return time.ToString("MMM dd, yyyy");
+        }
+    }
+}
---
+++ CommentEverythingMarketToolsNETCore/OptionSymbolConversion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CommentEverythingMarketToolsNETCore
+{
+    static class OptionSymbolConversion {
+        public enum OptionType {
+            PUT,
+            CALL
+        }
+
+        public static string CreateCBOEOptionSymbol(string underlyingSymbol, string expiry_yyMMdd, OptionType optionType, decimal strikePrice) {
+            // --- Pad price with leading zeroes, and to tenth of a penny
+            string priceStr = (strikePrice * 1000).ToString("0");
+            priceStr = priceStr.PadLeft(8, '0');
+
+            // --- Default to "C"
+            string putOrCall = "C";
+            if (optionType == OptionType.PUT) {
+                putOrCall = "P";
+            }
+
+            return underlyingSymbol + expiry_yyMMdd + putOrCall + priceStr;
+        }
+
+        public static string GetUnderlyingSymbolFromCBOEOption(string cboeOptionString) {
+            return cboeOptionString.Substring(0, cboeOptionString.Length - 15);
+        }
+
+        public static string GetFriendlyCBOEOptionString(string cboeOptionString) {
+            string underlyingSymbol = GetUnderlyingSymbolFromCBOEOption(cboeOptionString);
+            double expiryInUnixTime = ConvertCBOEExpiryStringToUnixTime(cboeOptionString.Substring(underlyingSymbol.Length, 6));
+            string prettyExpiryDate = Clock.DisplayDateString(Clock.Convert(expiryInUnixTime, Clock.TimeZoneId.UTC));
+            string optionType = cboeOptionString.Substring(underlyingSymbol.Length + 6, 1);
+            string strikePriceString = cboeOptionString.Substring(underlyingSymbol.Length + 7);
+            decimal strikePrice = decimal.Parse(strikePriceString) / 1000M;
+
+            return underlyingSymbol + " " + optionType + " " + prettyExpiryDate + " " + strikePrice.ToString(("0.00"));
+        }
+
+        public static double ConvertCBOEExpiryStringToUnixTime(string cboeExpiry) {
+            DateTime date = DateTime.ParseExact(cboeExpiry, "yyMMdd", CultureInfo.InvariantCulture);
+            return Clock.ConvertToUnixTimestamp(date, Clock.TimeZoneId.UTC);
+        }
+    }
+}
